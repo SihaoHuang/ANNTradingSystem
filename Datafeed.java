0254f32c8e052cb8509f3a472ee09b85a2f2ca59
Sihao Huang <astronomywong@gmail.com>
2016-12-29 15:09:08 -0500

    Start of Datafeed.java

---
+++ Datafeed.java
@@ -0,0 +1,14 @@
+public class Datafeed{
+
+  public Datafeed(){
+  }
+
+  public static Double getNewestPrice(String ticker)
+
+  public static Double getNewestVolume(String ticker)
+
+  public static Double getPE(String ticker)
+
+  public static Double getCap(String ticker)
+  
+}
