dc593890f036891e1cbf953ed2fd13bc3c0a672e
lorenzoschneiderman <lschneiderman@stuy.edu>
2017-01-07 21:53:23 -0500

    backpropagating algo

--- NeuralNetwork.java
+++ NeuralNetwork.java
@@ -9,9 +9,58 @@ class NeuralNetwork{
 
 	public static void main(String[] args) {
 		NeuralNetwork a = new NeuralNetwork();
-		a.initializeNet(10,7,2,5);
+		a.initializeNet(16,1,1,3);
 		System.out.println(a);
-		//a.initializeNet(100,15,60,25);
+		double[] t1 = {0.0,0.0,1.0};
+		double y1 = 0.0;
+		double[] t2 = {1.0,1.0,1.0};
+		double y2 = 1.0;
+		double[] t3 = {1.0,0.0,1.0};
+		double y3 = 1.0;
+		double[] t4 = {0.0,1.0,1.0};
+		double y4 = 0.0;
+
+		// Neuron finalNeuron = 
+		//System.out.println(a.network[network.length - 1][0]);
+		int iters = 1000;
+		int t = 300;
+		for (int i = 0; i < iters; i++){
+			//System.out.println(i);
+			a.feedData(t1,y1);
+			if (i % t == 0){
+				System.out.println(a.toStringOutLast());
+				System.out.println(y1);
+				System.out.println("--");
+			}
+
+			a.feedData(t2,y2);
+			if (i % t == 0){
+				System.out.println(a.toStringOutLast());
+				System.out.println(y2);
+				System.out.println("--");
+			}
+
+			a.feedData(t3,y3);
+			if (i % t == 0){
+				System.out.println(a.toStringOutLast());
+				System.out.println(y3);
+				System.out.println("--");
+			}
+
+			a.feedData(t4,y4);
+			if (i % t == 0){
+				System.out.println(a.toStringOutLast());
+				System.out.println(y4);
+				System.out.println("============================================");
+			}
+		}
+		// for (int layer = 1; layer < a.network.length; layer++){
+		// 	a.fireLayer(layer);
+		// }
+
+		// a.initializeNet();
+		//System.out.println(a.toStringOut());
+		// a.initializeNet(100,15,60,25);
 		
 		//System.out.println(a.getNeuron(0,0));	
 	}
@@ -44,22 +93,39 @@ class NeuralNetwork{
 			// inputNeuron.setOutput(example[i]);
 			network[0][i].setOutput(example[i]);         // input layer only contains neuron.output
 		}
-
+		// int iters = 1000;
+		// for (int i = 0; i < iters; i++){
 		for (int layer = 1; layer < network.length; layer++){
 			this.fireLayer(layer);
 		}
-
 		network[network.length - 1][0].backpropagate(target);    // backprop error in output
-		for (int layer = network.length -2; layer > 0; layer++){ // bug?
+		for (int layer = network.length - 2; layer >= 0; layer--){ // bug?                      //layer > 1 ?
 			this.backpropLayer(layer);
 		}
-		
-		for (int layer = 0; layer < network.length - 2; layer++){
+	
+		for (int layer = 0; layer < network.length - 1; layer++){
 			this.changeWeightLayer(layer);
 		}
+		// if (i% 250 == 0){
+			// System.out.println(this.toStringOut());
+		// }
+		// }
 
 	}
-
+	public String toStringOutLast(){
+		return ""+network[network.length - 1][0].getOutput();
+	}
+	public String toStringOut(){
+		String out = "";
+		for (int layer = 0; layer < network.length ; layer++){
+			for (Neuron neuron : network[layer]){
+				out += "["+Math.round(neuron.getOutput() * 1000)/1000.+"] ";
+			}
+			out += network[layer].length;
+			out += "\n";
+		}
+		return out;
+	}
 	public String toString(){
 		String out = "";
 		for (int layer = 0; layer < network.length ; layer++){
@@ -83,17 +149,17 @@ class NeuralNetwork{
 
 		//System.out.println(network[1].length);
 		this.connectLayerOut(0);
-		System.out.println("got here");
+
 		for (int layer = 1; layer < network.length - 1 ; layer++){
-			System.out.println(layer);
+
 			this.connectLayerIn(layer);
-			System.out.println("in");			
+			
 			this.connectLayerOut(layer);
-			System.out.println("out");
+
 	
 
 		}
-		this.connectLayerIn(network.length - 2);
+		this.connectLayerIn(network.length - 1);
 
 	}
 
@@ -110,14 +176,14 @@ class NeuralNetwork{
 
 	public void changeWeightLayer(int layer){
 		for (Neuron neuron : network[layer]){
-			neuron.adjustWeights(.001);
+			neuron.adjustWeights(.1);
 		}
 	}
 
 	public void connectLayerOut(int layer){            
 		for (Neuron neuron : network[layer]){
 			System.out.println(layer + 1);
-			Double weight = 1.;                              // change weights to random
+			Double weight = Math.random() * 2 - 1;                              // change weights to random
 			neuron.addOut(network[layer + 1], weight);
 		}
 	}	
@@ -131,5 +197,4 @@ class NeuralNetwork{
 	public Neuron getNeuron(int row, int col){
 		return network[row][col];
 	}
-
 }
